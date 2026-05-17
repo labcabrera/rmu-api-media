46790b895880c7029855f47629f2d1c6a12576b0
labcabrera <lab.cabrera@gmail.com>
2026-05-17 10:14:02 +0200

    Updated image categories.

--- src/modules/images/domain/entities/image-category.ts
+++ src/modules/images/domain/entities/image-category.ts
@@ -1,3 +1,16 @@
-export const IMAGE_CATEGORIES = ['generic', 'race', 'item', 'user', 'avatars'] as const;
+export const IMAGE_CATEGORIES = [
+  'actions',
+  'avatars',
+  'backgrounds',
+  'doc',
+  'generic',
+  'icons',
+  'items',
+  'npcs',
+  'photos',
+  'professions',
+  'races',
+  'user-data',
+] as const;
 
 export type ImageCategory = (typeof IMAGE_CATEGORIES)[number];
